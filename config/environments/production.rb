db1d3054dc2ba3d9eaff1965295d25309d814146
zunovia <kaneda.ryota@gmail.com>
2023-07-09 16:06:00 +0000

    production.rb refixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = Uglifier.new(harmony: true) #変更後
+  # config.assets.css_compressor = Uglifier.new(harmony: true) #変更後20230710 結局間違えてた。
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
